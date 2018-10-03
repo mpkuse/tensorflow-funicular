78e3e2fede374ff33035c3dd442a5f60007d6819
mpkuse <mpkuse@connect.ust.hk>
2018-10-03 15:44:54 +0800

    keras samples adaptation for cifar and mnist dataset

--- 2_mnist_conv_keras.py
+++ 2_mnist_conv_keras.py
@@ -1,4 +1,5 @@
 # Adopted from : https://github.com/Hvass-Labs/TensorFlow-Tutorials
+# Also see https://github.com/keras-team/keras/tree/master/examples
 
 import numpy as np
 # import matplotlib.pyplot as plt
@@ -9,95 +10,7 @@ import cv2
 import math
 import matplotlib.pyplot as plt
 
-def labels_to_logits( labels ):
-    n_classes = len( np.unique( labels ) )
-    logits = np.zeros( (labels.shape[0], n_classes) )
-
-    for i in range( len(labels) ):
-        logits[i, labels[i] ] = 1.
-    return logits
-
-def cmap_imshow( im ):
-    assert( len(im.shape) == 2 )
-    w_min = im.min()
-    w_max = im.max()
-
-    im_gray = ((im - w_min) / (w_max - w_min) * 255).astype('uint8')
-    cmap = cv2.applyColorMap(im_gray, cv2.COLORMAP_HOT)
-    cmap = cv2.resize(cmap, (0,0), fx=20, fy=20,  interpolation=cv2.INTER_NEAREST)
-
-
-    return cmap
-
-
-def plot_conv_weights(weights, input_channel=0):
-    # Get the lowest and highest values for the weights.
-    # This is used to correct the colour intensity across
-    # the images so they can be compared with each other.
-    w_min = np.min(weights)
-    w_max = np.max(weights)
-
-    # Number of filters used in the conv. layer.
-    num_filters = weights.shape[3]
-
-    # Number of grids to plot.
-    # Rounded-up, square-root of the number of filters.
-    num_grids = int( math.ceil(math.sqrt(num_filters)) )
-
-    # Create figure with a grid of sub-plots.
-    # code.interact( local=locals() )
-    fig, axes = plt.subplots(num_grids, num_grids)
-
-    # Plot all the filter-weights.
-    for i, ax in enumerate(axes.flat):
-        # Only plot the valid filter-weights.
-        if i<num_filters:
-            # Get the weights for the i'th filter of the input channel.
-            # See new_conv_layer() for details on the format
-            # of this 4-dim tensor.
-            img = weights[:, :, input_channel, i]
-
-            # Plot image.
-            ax.imshow(img, vmin=w_min, vmax=w_max,
-                      interpolation='nearest', cmap='seismic')
-
-        # Remove ticks from the plot.
-        ax.set_xticks([])
-        ax.set_yticks([])
-
-    # Ensure the plot is shown correctly with multiple plots
-    # in a single Notebook cell.
-    plt.show()
-
-
-def plot_conv_output(values):
-    # Number of filters used in the conv. layer.
-    num_filters = values.shape[3]
-
-    # Number of grids to plot.
-    # Rounded-up, square-root of the number of filters.
-    num_grids = int( math.ceil(math.sqrt(num_filters)) )
-
-    # Create figure with a grid of sub-plots.
-    fig, axes = plt.subplots(num_grids, num_grids)
-
-    # Plot the output images of all the filters.
-    for i, ax in enumerate(axes.flat):
-        # Only plot the images for valid filters.
-        if i<num_filters:
-            # Get the output image of using the i'th filter.
-            img = values[0, :, :, i]
-
-            # Plot image.
-            ax.imshow(img, interpolation='nearest', cmap='binary')
-
-        # Remove ticks from the plot.
-        ax.set_xticks([])
-        ax.set_yticks([])
-
-    # Ensure the plot is shown correctly with multiple plots
-    # in a single Notebook cell.
-    plt.show()
+from viz_utils import labels_to_logits, cmap_imshow, plot_conv_weights, plot_conv_output
 
 
 #---------------------------------------------------------------------------
@@ -135,13 +48,13 @@ model.summary()
 
 #---------------------------------------------------------------------------
 # Save Learned Model
-if True:
+if False:
     print 'load pretrained model: model.keras'
     model.load_weights( 'model.keras' )
 
 #---------------------------------------------------------------------------
 # Compile and Run iterations
-if False:
+if True:
     optimizer = tf.keras.optimizers.Adam(lr=1e-5)
 
     model.compile(optimizer=optimizer,
@@ -164,7 +77,7 @@ if False:
 # Analyse Layers
 print 'len( model.layers ) : ', len( model.layers )
 for l in model.layers:
-    print l
+    print l, l.weights, l.input, l.output
 print '---'
 print 'len( model.get_weights() )', len( model.get_weights() )
 for w in model.get_weights():
