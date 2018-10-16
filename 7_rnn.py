e5e84553832653cae3c5bcc2926cf1c6080b3b42
Manohar Kuse <mpkuse@connect.ust.hk>
2018-10-16 16:35:47 +0800

    simple rnn/lstm example on text seq

---
+++ 7_rnn.py
@@ -0,0 +1,60 @@
+""" Learning RNNs. Based on Karpaty's blog post
+http://karpathy.github.io/2015/05/21/rnn-effectiveness/
+
+    Author  : Manohar Kuse <mpkuse@connect.ust.hk>
+    Created : 16th Oct, 1990
+"""
+
+
+import keras
+import numpy as np
+import code
+
+#---
+#--- Data
+#---
+learning_data = 'hello'
+h = [1,0,0,0]
+e = [0,1,0,0]
+l = [0,0,1,0]
+o = [0,0,0,1]
+nop = [1,1,1,1]
+
+# 4 data points for 'hello'
+x = []
+y = []
+# h ==> e
+x.append( [ h, nop, nop, nop ] )
+y.append( e )
+
+# he ==> l
+x.append( [h, e, nop, nop])
+y.append( l )
+
+# hel ==> l
+x.append( [h, e, l, nop])
+y.append( l )
+
+# hell ==> o
+x.append( [h, e, l, l])
+y.append( o )
+
+x = np.array( x )
+y = np.array( y )
+code.interact( local=locals() )
+
+#---
+#--- Construct Network
+#---
+model = keras.Sequential()
+model.add(keras.layers.LSTM(7, input_shape=(4, 4)))
+model.add(keras.layers.Dense(4, activation='softmax'))
+
+model.summary()
+keras.utils.plot_model( model, show_shapes=True )
+
+
+optimizer = keras.optimizers.RMSprop(lr=0.01)
+model.compile(loss='categorical_crossentropy', optimizer=optimizer)
+
+model.fit(x=x, y=y, batch_size=4, epochs=20 )
---
+++ 7_rnn_on_text.py
@@ -0,0 +1,76 @@
+""" Taken a corpus of text as input and predicts the next word """
+
+import keras
+import numpy as np
+import code
+import io
+
+
+#---
+#--- Get Remote File
+#---
+path = keras.utils.data_utils.get_file(
+    'nietzsche.txt',
+    origin='https://s3.amazonaws.com/text-datasets/nietzsche.txt')
+with io.open(path, encoding='utf-8') as f:
+    text = f.read().lower()#.replace( '\n', ' ')
+print('corpus length:', len(text))
+
+
+#---
+#--- Make Dataset
+#---
+chars = sorted( list( set( text ) ) )
+
+char_indices = dict((c, i) for i, c in enumerate(chars))
+indices_char = dict((i, c) for i, c in enumerate(chars))
+
+# Window of size W on the text
+W = 40
+x = []
+y = []
+for i in range( 0, len(text)-W, 30 ): # make this 3 on pc with larger memory
+    if i%10000 == 0:
+        print 'process char#', i , ' of ', len(text)
+    # print i, i+W
+    cur_text = text[i:i+W]
+    next_char = text[i+W]
+    next_char_1hot = np.zeros( len(chars) )
+    next_char_1hot[ char_indices[next_char]  ] = 1.
+
+
+    cur_text_1hot = []
+    for ch in cur_text:
+        ch_1hot = np.zeros( len(chars) )
+        ch_1hot[ char_indices[ch] ] = 1.
+
+        cur_text_1hot.append( ch_1hot )
+    cur_text_1hot = np.array( cur_text_1hot )
+
+    x.append( cur_text_1hot )
+    y.append( next_char_1hot )
+    # code.interact( local=locals() )
+    # quit()
+x = np.array( x )
+y = np.array( y )
+print 'x.shape', x.shape
+print 'y.shape', y.shape
+
+#---
+#--- Construct Network
+#---
+model = keras.Sequential()
+model.add(keras.layers.LSTM(128, input_shape=( x.shape[1], x.shape[2])))
+model.add(keras.layers.Dense( x.shape[2], activation='softmax'))
+
+
+model.summary()
+keras.utils.plot_model( model, show_shapes=True )
+
+#---
+#--- Fit
+#---
+optimizer = keras.optimizers.RMSprop(lr=0.01)
+model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'] )
+model.fit(x=x, y=y, batch_size=256, epochs=20, validation_split=0.1 )
+model.save('model_text.keras')
